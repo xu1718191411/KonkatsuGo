8cbb52f5b2919f18b3eb22d583c5f2ffea14b518
xu1718191411 <xu1718191411@gmail.com>
2018-09-23 00:10:58 +0900

    give my config

---
+++ docs/konkatsu/public/javascripts/config.js
@@ -0,0 +1,10 @@
+var configData = {
+    config:{
+        apiKey: "AIzaSyBId_rgRqnyKqDB7qXLcTcH0c0uS7Ai-VY",
+        authDomain: "lunchambition-1504446899161.firebaseapp.com",
+        databaseURL: "https://lunchambition-1504446899161.firebaseio.com",
+        projectId: "lunchambition-1504446899161",
+        storageBucket: "lunchambition-1504446899161.appspot.com",
+        messagingSenderId: "828651070150"
+      }
+}
