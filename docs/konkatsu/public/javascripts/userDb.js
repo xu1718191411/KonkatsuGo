f920ceac2a0e530a28ff43689eb20b884f84e0b9
xu1718191411 <xu1718191411@gmail.com>
2018-09-22 21:44:04 +0900

    マッチング成功まで実装しました

--- docs/konkatsu/public/javascripts/userDb.js
+++ docs/konkatsu/public/javascripts/userDb.js
@@ -2,6 +2,7 @@
 
 var userDb = {
     COLLECTION: "users",
+    COLLECTIONCOMPLIMENT:"compliment",
     db: null,
 
     test_users: [{
@@ -44,12 +45,12 @@ var userDb = {
     // 特定UIDのユーザー情報取得
     getUserData: function (uid,cb) {
         this.db.collection(this.COLLECTION).doc(uid).get().then(function(res){
-            cb(res.data())
+            cb(res)
         })
     },
 
     // ユーザー情報セット
-    setUserData: function (uid, data) {
+    setUserData: function (uid, data,cb) {
         data.timestamp = new Date();
         // console.log('setUserData :');
         // console.log(uid);
@@ -57,19 +58,79 @@ var userDb = {
         this.db.collection(this.COLLECTION).doc(uid).set(data)
             .then(function () {
                 console.log("Document successfully written!");
+                cb(true)
             })
             .catch(function (error) {
                 console.error("Error writing document: ", error);
+                cb(false)
             });
     },
 
     // 範囲内のユーザー情報取得
-    getAreaUsers: function (parm, callback) {
+    getAreaUsers: function (parm, cb) {
         this.db.collection(this.COLLECTION)
             .get()
             .then(function (querySnapshot) {
-                callback(querySnapshot);
+                cb(querySnapshot);
             });
     },
 
+    compliment:function(uid,target,cb){
+
+        userDb.checkComliment(uid,target,function(shouldDoTheCompliment){
+            if(shouldDoTheCompliment){
+                userDb.doComliment(uid,target,function(error){
+                    if(error == null){
+                        cb(true)
+                        console.log("conguraduation!you have given a successful comliment")
+                    }else{
+                        
+                        console.log(error)
+                    }
+                })
+            }else{
+                cb(false)
+            }
+        })
+    },
+
+    checkComliment:function(uid,target,cb){
+        this.db.collection(this.COLLECTIONCOMPLIMENT).where("uid","==",uid).where("target","==",target).get().then(function(querySnapshot){
+
+            if(!querySnapshot.empty){
+                cb(false)
+                console.log("checkComliment:not empty")
+            }else{
+                cb(true)
+                console.log("checkComliment:empty")
+                //not yet give a comliment
+            }
+        }).catch(function(error){
+            cb(false)
+            console.log("checkComliment:error")
+        })
+    },
+
+    doComliment:function(uid,target,cb){
+        this.db.collection(this.COLLECTIONCOMPLIMENT).add({
+            uid:uid,
+            target:target
+        }).then(function(){
+            console.log("doComliment"+"success")
+            cb(null)
+        }).catch(function(error){
+            console.log("doComliment"+"failure")
+            cb(error)
+        })
+    },
+
+    checkMatching:function(uid,target,cb){
+        userDb.checkComliment(uid,target,function(hasDoneTheCompliment){
+                if(hasDoneTheCompliment){
+                    userDb.checkComliment(target,uid,function(successMatched){
+                       cb(true)
+                    })
+                }
+        })
+    }
 }
